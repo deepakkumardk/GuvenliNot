064ecaa3d7a46679ec37c1c00d08ac57953198e2
hakkikaancaliskan <caliskanhkaan@gmail.com>
2018-09-17 16:55:29 +0300

    fixed a problem
    problem was in about.kt
    logo was hidden

--- app/src/main/java/com/kaancaliskan/guvenlinot/About.kt
+++ app/src/main/java/com/kaancaliskan/guvenlinot/About.kt
@@ -1,14 +1,14 @@
 package com.kaancaliskan.guvenlinot
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.text.method.LinkMovementMethod
 import kotlinx.android.synthetic.main.about.*
 /**
  * This class is my about page and provides link to be clickable without showing whole link.
  */
-class About: Activity(){
+class About: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.about)
