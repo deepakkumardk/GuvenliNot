7cfc0a4b070283b5475676521d78461fc2b21cae
hakkikaancaliskan <caliskanhkaan@gmail.com>
2018-09-18 20:40:26 +0300

    converted LicenseActivity to Kotlin

---
+++ app/src/main/java/com/kaancaliskan/guvenlinot/LicenseActivity.kt
@@ -0,0 +1,32 @@
+package com.kaancaliskan.guvenlinot
+
+import android.content.Context
+import android.os.Bundle
+import android.view.MenuItem
+
+import com.danielstone.materialaboutlibrary.model.MaterialAboutList
+
+class LicenseActivity : AboutActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme_MaterialAboutActivity)
+    }
+
+    override fun getMaterialAboutList(context: Context): MaterialAboutList {
+        return AboutActivity.createMaterialAboutLicenseList(context)
+    }
+
+    override fun getActivityTitle(): CharSequence? {
+        return getString(R.string.licenses)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> return false
+        }
+    }
+}
