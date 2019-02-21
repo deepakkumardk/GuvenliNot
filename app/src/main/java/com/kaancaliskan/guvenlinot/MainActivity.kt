97522e66927f3e86a8d6bf10cef4eb7628fc12f4
Hakkı Kaan Çalışkan <caliskanhkaan@gmail.com>
2019-02-21 16:56:47 +0000

    Revert "Merge pull request #12 from deepakkumardk/master"
    This reverts commit c60d7321819eaec8b5608bdcdf910b0d52b8a5fd, reversing
    changes made to 58b5e2e89bd8645e4d65e118be14a9fbb0d35c2b.

--- app/src/main/java/com/kaancaliskan/guvenlinot/MainActivity.kt
+++ app/src/main/java/com/kaancaliskan/guvenlinot/MainActivity.kt
@@ -6,7 +6,6 @@ import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View.*
 import android.widget.Toast
 import com.kaancaliskan.guvenlinot.db.GuvenliNotDatabase
 import com.kaancaliskan.guvenlinot.db.Note
@@ -36,7 +35,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         noteList = NotesRepository(application).getAllNotes()
-        isListEmpty()
 
         val itemDecoration = DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL)
         recycler_view.addItemDecoration(itemDecoration)
@@ -64,20 +62,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun refreshRecyclerView() {
         noteList = NotesRepository(application).getAllNotes()
-        isListEmpty()
         adapter = GuvenliNotAdapter(noteList) { note -> onItemClick(note) }
         recycler_view.adapter = adapter
         adapter.notifyDataSetChanged()
-    }
-
-    private fun isListEmpty() {
-        if (noteList.isEmpty()) {
-            recycler_view.visibility = GONE
-            empty_view.visibility = VISIBLE
-        } else {
-            recycler_view.visibility = VISIBLE
-            empty_view.visibility = GONE
-        }
     }
 
     override fun onBackPressed() {
