b86f89b3db3dfbec583ca5da4f4d3b58685b30a8
Hakkı Kaan Çalışkan <caliskanhkaan@gmail.com>
2019-05-12 01:09:14 +0300

    update deps

--- app/src/main/java/com/kaancaliskan/guvenlinot/LoginActivity.kt
+++ app/src/main/java/com/kaancaliskan/guvenlinot/LoginActivity.kt
@@ -19,8 +19,6 @@ var check_for_intent = false
  */
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val hashedCurrentPassword = LocalData.read(this, getString(R.string.hashed_password))
-
         when (LocalData.read(this, getString(R.string.night_mode))) {
             "true" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             "false" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
@@ -31,32 +29,22 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         setSupportActionBar(login_bar)
 
-        /**
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        contentView!!.systemUiVisibility =
-        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        }
-        Ready to android Q :)
-         */
+        val hashedCurrentPassword = LocalData.read(this, getString(R.string.hashed_password))
         if (LocalData.read(this, getString(R.string.hashed_password)) == null) {
-            startActivity<FirstLogin>()
-            finish()
+            intent(2)
         }
         confirm_password_fab.setOnClickListener {
             val hashedPassword = Hash.sha512(confirm_EditText.text.toString())
             if (hashedPassword == hashedCurrentPassword) {
-                check_for_intent = true // For restrict accessing without password check.
-                startActivity<MainActivity>()
-                finish()
+                intent(0)
             } else {
                 confirm_layout.error = getString(R.string.password_check_error)
             }
         }
-        confirm_EditText.requestFocus()
     }
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_change_password -> {
-            startActivity<ChangePassword>()
+            intent(1)
             true
         }
         R.id.theme -> {
@@ -95,5 +83,19 @@ class LoginActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu, menu)
         menu.removeItem(R.id.action_search)
         return true
+    }
+    private fun intent(i: Int) {
+        when (i) {
+            0 -> {
+                check_for_intent = true
+                startActivity<MainActivity>()
+                finish()
+            }
+            1 -> startActivity<ChangePassword>()
+            2 -> {
+                startActivity<FirstLogin>()
+                finish()
+            }
+        }
     }
 }
